d772e1ddec2f78e0fbc0f241a4ade2221ae3b6db
dhiaul <dhiaulm@gmail.com>
2020-01-21 11:10:58 +0700

    Initial Commit Belajar Websocket channel di Django

---
+++ chat/urls.py
@@ -0,0 +1,6 @@
+from django.conf.urls import url, include
+from . import views
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    url(r'^(?P<room_name>[\w-]+)/$', views.room, name='room'),
+]
